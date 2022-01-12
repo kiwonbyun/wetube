241a60564e42040bec70bf709bb8d3f35baa880e
bkw9603 <bkw9603@naver.com>
2022-01-13 02:53:27 +0900

    22.1.12 Router learning

--- src/server.js
+++ src/server.js
@@ -1,20 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("i love middlewares!");
-};
-
+const logger = morgan("dev");
 app.use(logger);
 
-app.get("/", handleHome);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.listen(PORT, () =>
   console.log(`✅server listening on port http://localhost:${PORT}}🚀`)
