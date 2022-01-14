209190accdae9eeda0bdb0d721d3d7f8c4ce3322
bkw9603 <bkw9603@naver.com>
2022-01-14 19:30:18 +0900

    22.1.14 template learning

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,34 @@
-export const trending = (req, res) => res.send("trending Videos");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      name: "First video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      name: "second video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      name: "third video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "home", videos });
 };
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit");
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deletevideo = (req, res) => {
