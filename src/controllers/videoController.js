bbee961de7e758d29d708d406fa221051f1424e5
bkw9603 <bkw9603@naver.com>
2022-01-16 23:50:18 +0900

    22.1.16 PUG iteration, conditionals learning

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,38 @@
+let videos = [
+  {
+    title: "First video,xman",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 2,
+    id: 1,
+  },
+  {
+    title: "second video,iron man",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "third video,thor",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      name: "First video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      name: "second video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      name: "third video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
-  res.render("home", { pageTitle: "home", videos });
+  return res.render("home", { pageTitle: "home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { see, edit, upload, deletevideo } from "../controllers/videoController";
+import {
+  see,
+  edit,
+  upload,
+  deletevideo,
+  watch,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deletevideo);
 
