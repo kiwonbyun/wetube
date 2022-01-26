2c3470a6eb7b6ed4b078f7aa1da57b9154fa0532
bkw9603 <bkw9603@naver.com>
2022-01-26 15:56:10 +0900

    22.1.26 scss learning

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,9 @@ import Video from "../models/video";
 import User from "../models/User";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "home", videos });
 };
 export const watch = async (req, res) => {
